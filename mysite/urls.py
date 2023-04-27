75a3f414c706aeda06447fbdd506c99b703ce880
MuralikrishnaShetty <workformuralikrishnashetty@gmail.com>
2023-04-27 16:25:25 +0530

    deploy

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include 
+# from django.views.static import serve
+# from django.conf.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authenticate.urls')),
+    
 ]
